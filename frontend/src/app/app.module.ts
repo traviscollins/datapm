b0f691c315eacb56c4ac48eea19e2bb394f14258
Travis Collins <travis@dreamingwell.com>
2020-08-29 22:48:04 -0400

    Revert "Removed modules"
    This reverts commit 26c2b7c6a5b002ad48ca5ae5bce43e0af1012180.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { CatalogDetailsComponent } from './catalog-details/catalog-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CatalogDetailsComponent
   ],
   imports: [
     BrowserModule,
